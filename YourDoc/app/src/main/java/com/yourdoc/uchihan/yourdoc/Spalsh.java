603aa3fdfa259ccd54fa876acfc5a0a903dfa3b9
ahmed <forstudying76@gmail.com>
2017-08-06 00:35:09 +0300

    2init

---
+++ YourDoc/app/src/main/java/com/yourdoc/uchihan/yourdoc/Spalsh.java
@@ -0,0 +1,13 @@
+package com.yourdoc.uchihan.yourdoc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Spalsh extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh);
+    }
+}
