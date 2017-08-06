54bcccbf0b3faa433932db75c6d659e6ac21317f
Tech-world <Gemo@GeMoOo.local>
2017-08-06 02:14:32 -0700

    first expandable Recycler view

---
+++ Your Doc/app/src/main/java/com/ahmed/yourdoc/Constant.java
@@ -0,0 +1,11 @@
+package com.ahmed.yourdoc;
+
+/**
+ * Created by ksunil on 12/23/16.
+ */
+
+public class Constant {
+
+    public static String [] name = {"Android", "iOS"};
+    public static String [] subName = {"google", "Motorola", "Samsung", "Lenevo"};
+}
