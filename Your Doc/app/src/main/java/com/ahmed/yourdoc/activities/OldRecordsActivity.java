52b0df77289a0de8cf8d59f4a38260a4aeb94846
ahmed <forstudying76@gmail.com>
2017-09-30 15:57:04 +0300

    before ol records

---
+++ Your Doc/app/src/main/java/com/ahmed/yourdoc/activities/OldRecordsActivity.java
@@ -0,0 +1,15 @@
+package com.ahmed.yourdoc.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ahmed.yourdoc.R;
+
+public class OldRecordsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_old_records);
+    }
+}
